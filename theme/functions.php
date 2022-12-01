70a11b10364931359c4592024915c252e6ec5fd1
kiwi <winterwind.sh0206@gmail.com>
2022-12-01 11:16:24 +0900

    不要な機能を削除

--- theme/functions.php
+++ theme/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wp_shortlink_wp_head');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+
 if ( ! function_exists( 'kiwidev_theme_setup' ) ) {
     /**
      * Sets up theme defaults and registers support for various WordPress features.
