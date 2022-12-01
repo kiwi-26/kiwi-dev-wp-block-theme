bb615f19db82b87a4403cee0562d4da175036111
kiwi <winterwind.sh0206@gmail.com>
2022-12-01 11:16:44 +0900

    タイトルタグ、カスタムロゴをサポートに追加

--- theme/functions.php
+++ theme/functions.php
@@ -17,6 +17,9 @@ if ( ! function_exists( 'kiwidev_theme_setup' ) ) {
      */
     function kiwidev_theme_setup() {
         // Add supports
+        add_theme_support( 'title-tag' );
+		add_theme_support( 'custom-logo' );
+        
         add_theme_support( 'wp-block-styles' );
 
         /*
