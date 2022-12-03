6dcd9e796ebf20dcb4c304f0293aa0f075b1d111
kiwi <winterwind.sh0206@gmail.com>
2022-12-03 16:50:31 +0900

    パターンサンプルとして投稿メタ情報のブロックを追加し、単一テンプレートから呼び出し

---
+++ theme/patterns/post-meta.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Title: 投稿メタ情報
+ * Description: 投稿日、投稿者、カテゴリー、タグを2カラムで表示します
+ * Slug: kiwi-dev-wp-block-theme/post-meta
+ * Categories: featured, columns
+ * Keywords: Post meta, 投稿 メタ
+ * Block Types: core/columns
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns">
+    <!-- wp:column -->
+    <div class="wp-block-column">
+        <!-- wp:post-date /-->
+        <!-- wp:post-author /-->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column -->
+    <div class="wp-block-column">
+        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group">
+            <!-- wp:paragraph -->
+                <p><strong>カテゴリー</strong></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:post-terms {"term":"category"} /-->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group">
+            <!-- wp:paragraph -->
+                <p><strong>タグ</strong></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:post-terms {"term":"post_tag"} /-->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
